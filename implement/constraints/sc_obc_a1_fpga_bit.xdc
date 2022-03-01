bd558a91676fade7159ae8aa55b0b30ce7778ec8
Daisuke Sasaki <daisuke@spacecubics.com>
2022-03-01 09:15:10 +0900

    implement: Add SC-OBC-A1 generate bitstream enviroment

---
+++ implement/constraints/sc_obc_a1_fpga_bit.xdc
@@ -0,0 +1,11 @@
+# --------------------------------------------------
+# Space Cubics OBC A1 (SC-OBC-A1)
+# FPGA Bitstream export format file (.xdc)
+#  sc_obc_a1_fpga_bit.tcl
+#  Copyright © 2022 Space Cubics, LLC.
+# --------------------------------------------------
+
+set_property BITSTREAM.CONFIG.UNUSEDPIN PULLNONE [current_design]
+set_property BITSTREAM.CONFIG.SPI_BUSWIDTH  4 [current_design];
+set_property BITSTREAM.CONFIG.CONFIGRATE   50 [current_design];
+set_property BITSTREAM.GENERAL.COMPRESS  TRUE [current_design];
---
+++ implement/tcl/gen_bitstream.tcl
@@ -0,0 +1,40 @@
+# --------------------------------------------------
+# Space Cubics OBC A1 (SC-OBC-A1)
+# sc_obc_a1_fpga Generate bitstream script
+#  gen_bitstream.tcl
+#  Copyright © 2022 Space Cubics, LLC.
+# --------------------------------------------------
+
+source tcl/set_environment.tcl
+source tcl/set_device.tcl
+
+# check argument
+if {${argc} < 2} {
+    puts "Not enough arguments."
+    puts " vivado -mode tcl -source (script) -tclargs (TOP Module) (Route Design Check Point)"
+    exit 1
+}
+set arglist ${argv}
+
+# set top module
+set topmodule [lindex ${arglist} 0]
+puts "TOP Module: ${topmodule}"
+set arglist [lreplace ${arglist} 0 0];
+
+# set design checkpoint
+set dcpfile [lindex ${arglist} 0]
+puts "Design Check Point: ${dcpfile}"
+set arglist [lreplace ${arglist} 0 0];
+
+# Read Checkpoint
+read_checkpoint ${dcpfile}
+link_design -name ${topmodule} -part ${xil_part} -top ${topmodule}
+
+# Read xdc file
+read_xdc ${xdcd}/${topmodule}_bit.xdc
+
+# Write bitstream
+write_bitstream -force ${bitd}/${topmodule}.bit
+write_cfgmem -force -format MCS -interface SPIx4 -size 16 -loadbit "up 0x0 ${bitd}/${topmodule}.bit" -file ${bitd}/${topmodule}.mcs
+
+exit 0
