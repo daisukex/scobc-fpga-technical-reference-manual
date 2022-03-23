6da956456e5e2baace9dd5078890b558f211fa11
Daisuke Sasaki <daisuke@spacecubics.com>
2022-03-23 12:34:05 +0900

    sc-veri-lib: Add sclib_tmr_ff_winit (TMR F/F with Initial Value)

---
+++ rtl/sc-veri-lib/sclib_tmr_ff_winit.v
@@ -0,0 +1,52 @@
+//-----------------------------------------------
+// Space Cubics Standard IP Core
+//  Triple modular redundancy Flip Flop with  Initial Value
+//  Module: sclib_tmr_ff_winit
+//-----------------------------------------------
+// Copyright © 2022 Space Cubics, LLC.
+//-----------------------------------------------
+
+module sclib_tmr_ff_winit # (
+  parameter DW = 1,      // Data Width
+  parameter IVAL = 1'b0, // Initial Value
+  parameter SRVAL = 1'b0 // SetReset Value
+) (
+  input [DW-1:0] D,
+  input CLK,
+  input SRB,
+  output reg [DW-1:0] Q
+);
+
+(* dont_touch = "yes" *) reg [DW-1:0] d_tmr_0 = {DW{IVAL}};
+(* dont_touch = "yes" *) reg [DW-1:0] d_tmr_1 = {DW{IVAL}};
+(* dont_touch = "yes" *) reg [DW-1:0] d_tmr_2 = {DW{IVAL}};
+
+always @ (posedge CLK or negedge SRB) begin
+  if (!SRB) begin
+    d_tmr_0 <= SRVAL;
+    d_tmr_1 <= SRVAL;
+    d_tmr_2 <= SRVAL;
+  end
+  else begin
+    d_tmr_0 <= {DW{D}};
+    d_tmr_1 <= {DW{D}};
+    d_tmr_2 <= {DW{D}};
+  end
+end
+
+genvar b;
+generate
+  for (b=0; b<DW; b=b+1) begin: mvote
+    always @ (*) begin
+      case ({d_tmr_2[b], d_tmr_1[b], d_tmr_0[b]})
+        3'b011: Q[b] = 1'b1;
+        3'b101: Q[b] = 1'b1;
+        3'b110: Q[b] = 1'b1;
+        3'b111: Q[b] = 1'b1;
+        default: Q[b] = 1'b0;
+      endcase
+    end
+  end
+endgenerate
+
+endmodule
