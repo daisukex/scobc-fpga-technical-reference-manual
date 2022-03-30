0e138e6daf0f97598f7f8e08444397d08c2dcd87
Daisuke Sasaki <daisuke@spacecubics.com>
2022-03-31 01:27:26 +0900

    tb: testcase: Update check_sysreg

--- tb/testcase/check_sysreg.sv
+++ tb/testcase/check_sysreg.sv
@@ -40,53 +40,90 @@ initial begin
   @ (negedge CMC_ACK);
   display_text("SC-OBC-CORE System Bootup Done", 1, 1);
 
+  repeat (10) @(posedge SYS_CLK);
   //--------------------------------------------------
-  label    = "Check System Register: CFGMEMCTL";
+  label    = "Check System Register: VERSION";
   simcount = 1;
   //--------------------------------------------------
+  display_subcount_text(1, "Check Version Initial Value", 1);
+  read_transaction( .master(2), .addr(`SYSREG_BASE+`SYSREG_VER), .expdata(32'h0001_0001), .check(1));
+
+
+  //--------------------------------------------------
+  label    = "Check System Register: CFGMEMCTL";
+  simcount = 2;
+  //--------------------------------------------------
   display_subcount_text(1, "Check CFGITCMEN Initial Value", 1);
-  read_transaction( .master(2), .addr(`SYSREG_BASE+`SYSREG_CFGMEMCTL), .expdata(1<<`SYSREG_ITCMEN), .check(1));
+  read_transaction( .master(2), .addr(`SYSREG_BASE+`SYSREG_CODEMSEL), .expdata(1<<`SR_ITCMEN), .check(1));
 
   display_subcount_text(2, "Change CFGITCMEN: 1'b0", 1);
-  write_transaction(.master(2), .addr(`SYSREG_BASE+`SYSREG_CFGMEMCTL), .data(0<<`SYSREG_ITCMEN));
-  read_transaction( .master(2), .addr(`SYSREG_BASE+`SYSREG_CFGMEMCTL), .expdata(1<<`SYSREG_ITCMEN), .check(1));
-  write_transaction(.master(2), .addr(`SYSREG_BASE+`SYSREG_CFGMEMCTL), .data(16'h5A5A<<`SYSREG_CFGMEMCTLPKC |
-                                                                             0<<`SYSREG_ITCMEN));
+  write_transaction(.master(2), .addr(`SYSREG_BASE+`SYSREG_CODEMSEL), .data(0<<`SR_ITCMEN));
+  read_transaction( .master(2), .addr(`SYSREG_BASE+`SYSREG_CODEMSEL), .expdata(1<<`SR_ITCMEN), .check(1));
+  write_transaction(.master(2), .addr(`SYSREG_BASE+`SYSREG_CODEMSEL), .data(16'h5A5A<<`SR_ITCMENPKC | 0<<`SR_ITCMEN));
   @ (posedge SYS_RSTB);
   repeat (100) @(posedge SYS_CLK);
-  read_transaction( .master(2), .addr(`SYSREG_BASE+`SYSREG_CFGMEMCTL), .expdata(0<<`SYSREG_ITCMEN), .check(1));
+  read_transaction( .master(2), .addr(`SYSREG_BASE+`SYSREG_CODEMSEL), .expdata(0<<`SR_ITCMEN), .check(1));
 
   display_subcount_text(3, "Change CFGITCMEN: 1'b1", 1);
-  write_transaction(.master(2), .addr(`SYSREG_BASE+`SYSREG_CFGMEMCTL), .data(0<<`SYSREG_ITCMEN));
-  read_transaction( .master(2), .addr(`SYSREG_BASE+`SYSREG_CFGMEMCTL), .expdata(0<<`SYSREG_ITCMEN), .check(1));
-  write_transaction(.master(2), .addr(`SYSREG_BASE+`SYSREG_CFGMEMCTL), .data(16'h5A5A<<`SYSREG_CFGMEMCTLPKC |
-                                                                             1<<`SYSREG_ITCMEN));
+  write_transaction(.master(2), .addr(`SYSREG_BASE+`SYSREG_CODEMSEL), .data(0<<`SR_ITCMEN));
+  read_transaction( .master(2), .addr(`SYSREG_BASE+`SYSREG_CODEMSEL), .expdata(0<<`SR_ITCMEN), .check(1));
+  write_transaction(.master(2), .addr(`SYSREG_BASE+`SYSREG_CODEMSEL), .data(16'h5A5A<<`SR_ITCMENPKC | 1<<`SR_ITCMEN));
   @ (posedge SYS_RSTB);
   repeat (100) @(posedge SYS_CLK);
-  read_transaction( .master(2), .addr(`SYSREG_BASE+`SYSREG_CFGMEMCTL), .expdata(1<<`SYSREG_ITCMEN), .check(1));
+  read_transaction( .master(2), .addr(`SYSREG_BASE+`SYSREG_CODEMSEL), .expdata(1<<`SR_ITCMEN), .check(1));
 
 
   repeat (10) @(posedge SYS_CLK);
   //--------------------------------------------------
   label    = "Check System Register: SYSCLKCTL";
-  simcount = 2;
+  simcount = 3;
   //--------------------------------------------------
   @(posedge SYS_CLK);
-  display_subcount_text(1, "Check CLKMODE Initial Value", 1);
-  read_transaction( .master(2), .addr(`SYSREG_BASE+`SYSREG_SYSCLKCTL), .expdata(2'b10<<`SYSREG_CLKMODE), .check(1));
-  repeat (100) @(posedge SYS_CLK);
+  display_subcount_text(1, "Check CLKMODE Initial Value 2'b01", 1);
+  read_transaction( .master(2), .addr(`SYSREG_BASE+`SYSREG_SYSCLKCTL), .expdata(2'b01<<`SR_CLKMODE), .check(1));
+  repeat (10) @(posedge SYS_CLK);
 
-  display_subcount_text(2, "Change CLKMODE: 2'b01", 1);
-  write_transaction(.master(2), .addr(`SYSREG_BASE+`SYSREG_SYSCLKCTL), .data(2'b01<<`SYSREG_CLKMODE));
-  repeat (100) @(posedge SYS_CLK);
-  read_transaction( .master(2), .addr(`SYSREG_BASE+`SYSREG_SYSCLKCTL), .expdata(2'b01<<`SYSREG_CLKMODE), .check(1));
+  display_subcount_text(2, "Change CLKMODE: 2'b10", 1);
+  write_transaction(.master(2), .addr(`SYSREG_BASE+`SYSREG_SYSCLKCTL), .data(2'b10<<`SR_CLKMODE));
+  repeat (10) @(posedge SYS_CLK);
+  read_transaction( .master(2), .addr(`SYSREG_BASE+`SYSREG_SYSCLKCTL), .expdata(2'b10<<`SR_CLKMODE), .check(1));
 
   display_subcount_text(3, "Change CLKMODE: 2'b00", 1);
-  write_transaction(.master(2), .addr(`SYSREG_BASE+`SYSREG_SYSCLKCTL), .data(2'b00<<`SYSREG_CLKMODE));
+  write_transaction(.master(2), .addr(`SYSREG_BASE+`SYSREG_SYSCLKCTL), .data(2'b00<<`SR_CLKMODE));
   @ (negedge PLLLOCK);
-  read_transaction( .master(2), .addr(`SYSREG_BASE+`SYSREG_SYSCLKCTL), .expdata(2'b00<<`SYSREG_CLKMODE), .check(1));
-
+  read_transaction( .master(2), .addr(`SYSREG_BASE+`SYSREG_SYSCLKCTL), .expdata(2'b00<<`SR_CLKMODE), .check(1));
   repeat (10) @(posedge SYS_CLK);
+
+  display_subcount_text(4, "System Reconfiguration (FPGA_BOOT = 2'b10)", 1);
+  FPGA_BOOT = 2'b10;
+  system_reconfig();
+  skip_sram_init();
+  @ (posedge SYS_RSTB);
+  @ (posedge CMC_REQ);
+  @ (posedge CMC_ACK);
+  @ (posedge PLLLOCK);
+  @ (negedge CMC_REQ);
+  @ (negedge CMC_ACK);
+
+  display_subcount_text(5, "Check CLKMODE Initial Value 2'b01", 1);
+  read_transaction( .master(2), .addr(`SYSREG_BASE+`SYSREG_SYSCLKCTL), .expdata(2'b10<<`SR_CLKMODE), .check(1));
+  repeat (10) @(posedge SYS_CLK);
+
+  display_subcount_text(6, "System Reconfiguration (FPGA_BOOT = 2'b00)", 1);
+  FPGA_BOOT = 2'b00;
+  system_reconfig();
+  skip_sram_init();
+  @ (posedge SYS_RSTB);
+  @ (posedge CMC_REQ);
+  @ (posedge CMC_ACK);
+  @ (negedge CMC_REQ);
+  @ (negedge CMC_ACK);
+
+  display_subcount_text(7, "Check CLKMODE Initial Value 2'b00", 1);
+  read_transaction( .master(2), .addr(`SYSREG_BASE+`SYSREG_SYSCLKCTL), .expdata(2'b00<<`SR_CLKMODE), .check(1));
+  repeat (100) @(posedge SYS_CLK);
+
+
   //--------------------------------------------------
   label    = "Check System Register: SPAD1-SPAD4";
   simcount = 3;
@@ -109,13 +146,6 @@ initial begin
   read_transaction( .master(2), .addr(`SYSREG_BASE+`SYSREG_SPAD3), .expdata(32'h0B0A_0908), .check(1));
   read_transaction( .master(2), .addr(`SYSREG_BASE+`SYSREG_SPAD4), .expdata(32'h0F0E_0D0C), .check(1));
 
-  repeat (10) @(posedge SYS_CLK);
-  //--------------------------------------------------
-  label    = "Check System Register: VERSION";
-  simcount = 3;
-  //--------------------------------------------------
-  display_subcount_text(1, "Check Version Initial Value", 1);
-  read_transaction( .master(2), .addr(`SYSREG_BASE+`SYSREG_VER), .expdata(32'h0001_0001), .check(1));
 
   repeat (100) @ (posedge SYS_CLK);
   simfinish(0);
