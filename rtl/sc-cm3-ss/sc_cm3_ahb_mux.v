0f6344eeecdba1584b2687b3d1f903907bbf7045
Daisuke Sasaki <daisuke@spacecubics.com>
2022-04-08 16:50:08 +0900

    sc-cm3-ss: Update AHB Multiplexer
    The original logic had a problem when HREADY went low.
    
    In addition, I-Code and D-Code AHB were supposed to operate
    exclusively. But, when HREADY is Low, there are cases where it
    seems that requests are being received at the same time.
    
    So, we changed the circuit to receive and process requests from I-Code
    and D-Code in a double buffer.

--- rtl/sc-cm3-ss/sc_cm3_ahb_mux.v
+++ rtl/sc-cm3-ss/sc_cm3_ahb_mux.v
@@ -51,19 +51,20 @@ parameter IDL = 2'b00,
           NSQ = 2'b10,
           SEQ = 2'b11;
 
-reg ivalid;
-reg iburst;
-reg dvalid;
-reg dburst;
-reg [1:0] cycle_id;
-reg [1:0] htns [0:1];
-reg [31:0] hadr [0:1];
-reg [2:0] hbst [0:1];
-reg hwrt;
-reg [2:0] hsze [0:1];
-reg [3:0] hprt [0:1];
-reg [31:0] hwdt;
-reg hinvalid;
+reg [3:0] tbuf [0:1];
+reg tbwp, tbrp;
+reg [1:0]  tns [0:1];
+reg [31:0] adr [0:1];
+reg [2:0]  bst [0:1];
+reg [0:1]  wrt;
+reg [2:0]  sze [0:1];
+reg [3:0]  prt [0:1];
+reg [2:0] hchsel;
+reg [2:0] hdtval;
+reg mhadp;
+reg mhdtp;
+reg [1:0] mhresp;
+reg [31:0] mhrdata;
 
 wire itstart = (HTRANSI == NSQ);
 wire ittrans = (HTRANSI == SEQ);
@@ -73,238 +74,163 @@ wire dttrans = (HTRANSD == SEQ);
 wire dtidle  = (HTRANSD == IDL);
 
 // I-Code/D-Code AHB Controller
-always @ (posedge HCLK or negedge HRESETN) begin
+// ------------------------------
+always @ (posedge HCLK) begin
   if (!HRESETN) begin
-    ivalid <= 1'b0;
-    iburst <= 1'b0;
-    dvalid <= 1'b0;
-    dburst <= 1'b0;
-    HRDATAI <= 32'h0;
     HREADYI <= 1'b1;
-    HRESPI <= 2'b00;
-    HRDATAD <= 32'h0;
     HREADYD <= 1'b1;
-    HRESPD <= 2'b00;
+    tbuf[0] <= 4'b0000;
+    tbuf[1] <= 4'b0000;
+    tbwp <= 0;
+    tbrp <= 0;
+    hchsel <= 2'b00;
+    hdtval <= 2'b00;
   end
   else begin
-
-    if (itstart & HREADYI) begin
-      ivalid <= 1'b1;
-      if (HBURSTI != 3'b000)
-        iburst <= 1'b1;
-      else
-        iburst <= 1'b0;
+    // Address Queue
+    if (HREADYI & itstart) begin
+      tbuf[tbwp][1:0] <= 2'b01;
+      tbuf[tbwp][2] <= 1'b0;
+      tbuf[tbwp][3] <= HBURSTI != 3'b000;
+      tbwp <= ~tbwp;
+      HREADYI <= 1'b0;
     end
-    else if (cycle_id[0] & HREADY) begin
-      if (~iburst | (iburst & (itstart | itidle))) begin
-        iburst <= 1'b0;
-        ivalid <= 1'b0;
+    else if (HREADYD & dtstart) begin
+      tbuf[tbwp][1:0] <= 2'b10;
+      tbuf[tbwp][2] <= HWRITED;
+      tbuf[tbwp][3] <= HBURSTD != 3'b000;
+      tbwp <= ~tbwp;
+      HREADYD <= 1'b0;
+    end
+
+    if (HREADYI & (itstart | ittrans)) begin
+      tns[0] <= HTRANSI;
+      adr[0] <= HADDRI;
+      bst[0] <= HBURSTI;
+      wrt[0] <= 1'b0;
+      sze[0] <= HSIZEI;
+      prt[0] <= HPROTI;
+      HREADYI <= 1'b0;
+    end
+    else if (ittrans & HREADY)
+      tns[0] <= BSY;
+
+    if (HREADYD & (dtstart | dttrans)) begin
+      tns[1] <= HTRANSD;
+      adr[1] <= HADDRD;
+      bst[1] <= HBURSTD;
+      wrt[1] <= HWRITED;
+      sze[1] <= HSIZED;
+      prt[1] <= 4'b1111;
+      HREADYD <= 1'b0;
+    end
+    else if (dttrans & HREADY)
+      tns[1] <= BSY;
+
+    // Trans queue read pointer
+    if (HREADY) begin
+      if (mhadp & !tbuf[tbrp][3]) begin
+        tbrp <= ~tbrp;
+        tbuf[tbrp][1:0] <= 2'b00;
+      end
+      else if (mhadp & tbuf[tbrp][3] &
+             ((tbuf[tbrp][1:0] == 2'b01 & (itstart | itidle)) |
+              (tbuf[tbrp][1:0] == 2'b10 & (dtstart | dtidle)))) begin
+        tbrp <= ~tbrp;
+        tbuf[tbrp][1:0] <= 2'b00;
+      end
+      else if (mhadp & tbuf[~tbrp][1:0] != 2'b00) begin
+        tbrp <= ~tbrp;
+        tbuf[tbrp][1:0] <= 2'b00;
       end
     end
-    else if (iburst & (itstart | itidle))
-      iburst <= 1'b0;
 
-    if (itstart & HREADYI) begin
-      HREADYI <= 1'b0;
-      htns[0] <= HTRANSI;
-      hadr[0] <= HADDRI;
-      hbst[0] <= HBURSTI;
-      hsze[0] <= HSIZEI;
-      hprt[0] <= HPROTI;
-    end
-    else if (iburst & HREADYI) begin
-      htns[0] <= HTRANSI;
-      hadr[0] <= HADDRI;
-      hbst[0] <= HBURSTI;
-      hsze[0] <= HSIZEI;
-      hprt[0] <= HPROTI;
-    end
-
-    if (cycle_id[0] & hinvalid)
-      HREADYI <= 1'b0;
-    else if (cycle_id[0] & HREADY) begin
-      HRDATAI <= HRDATA;
-      HREADYI <= 1'b1;
-      HRESPI <= HRESP;
-    end
-
-    if (dtstart & HREADYD) begin
-      dvalid <= 1'b1;
-      if (HBURSTD != 3'b000)
-        dburst <= 1'b1;
-      else
-        dburst <= 1'b0;
-    end
-    else if (cycle_id[1] & HREADY) begin
-      if (~dburst | (dburst & (dtstart | dtidle))) begin
-        dvalid <= 1'b0;
-        dburst <= 1'b0;
+    if (HREADY) begin
+      hdtval <= 2'b00;
+      hchsel <= tbuf[tbrp][1:0];
+      if (mhdtp) begin
+        if (hchsel[0]) begin
+          hdtval[0] <= 1'b1;
+          HREADYI <= 1'b1;
+        end
+        if (hchsel[1]) begin
+          hdtval[1] <= 1'b1;
+          HREADYD <= 1'b1;
+        end
       end
-    end
-    else if (dburst & (dtstart | dtidle))
-      dburst <= 1'b0;
-
-    if (dtstart & HREADYD) begin
-      HREADYD <= 1'b0;
-      htns[1] <= HTRANSD;
-      hadr[1] <= HADDRD;
-      hbst[1] <= HBURSTD;
-      hwrt <= HWRITED;
-      hsze[1] <= HSIZED;
-      hprt[1] <= 4'b1111;
-    end
-    else if (dburst & HREADYD) begin
-      htns[1] <= HTRANSD;
-      hadr[1] <= HADDRD;
-      hbst[1] <= HBURSTD;
-      hwrt <= HWRITED;
-      hsze[1] <= HSIZED;
-      hprt[1] <= 4'b1111;
-    end
-
-    if (dvalid & hwrt)
-      hwdt <= HWDATAD;
-
-    if (cycle_id[1] & hinvalid)
-      HREADYD <= 1'b0;
-    else if (cycle_id[1] & HREADY) begin
-      HRDATAD <= HRDATA;
-      HREADYD <= 1'b1;
-      HRESPD <= HRESP;
     end
   end
 end
 
-// AHB Cycle Controller
-always @ (posedge HCLK or negedge HRESETN) begin
-  if (!HRESETN)
-    cycle_id <= 2'b00;
-  else begin
-    if (HREADY & ~hinvalid) begin
-      if (~cycle_id[0] & ivalid)
-        cycle_id[0] <= 1'b1;
-      else if ((~iburst & cycle_id[0]) | (iburst & (itstart | itidle)))
-        cycle_id[0] <= 1'b0;
-
-      if (~cycle_id[1] & dvalid)
-        cycle_id[1] <= 1'b1;
-      else if ((~dburst & cycle_id[1]) | (dburst & (dtstart | dtidle)))
-        cycle_id[1] <= 1'b0;
-    end
-  end
-end
-
-reg adphase;
-always @ (posedge HCLK or negedge HRESETN) begin
-  if (!HRESETN)
-    adphase <= 1'b0;
-  else if ((itstart | ittrans) & HREADYI)
-    adphase <= 1'b1;
-  else if ((dtstart | dttrans) & HREADYD)
-    adphase <= 1'b1;
-  else
-    adphase <= 1'b0;
-end
-
+// I-Code/D-Code AHB Signal Select
+// ----------------------------------------
 always @ (*) begin
-  HTRANS = IDL;
-  HADDR = 32'h0;
-  HBURST = 3'h0;
-  HWRITE = 1'b0;
-  HSIZE = 3'h0;
-  HPROT = 4'b1111;
-  HWDATA = 32'h0;
-  if (adphase) begin
-    if (cycle_id == 2'b00) begin
-      if (ivalid) begin
-        HTRANS = htns[0];
-        HADDR = hadr[0];
-        HBURST = hbst[0];
-        HWRITE = 1'b0;
-        HSIZE = hsze[0];
-        HPROT = hprt[0];
-      end
-      else if (dvalid) begin
-        HTRANS = htns[1];
-        HADDR = hadr[1];
-        HBURST = hbst[1];
-        HWRITE = hwrt;
-        HSIZE = hsze[1];
-        HPROT = hprt[1];
-      end
-    end
-
-    else if (cycle_id == 2'b01) begin
-      if (iburst) begin
-        HTRANS = htns[0];
-        HADDR  = hadr[0];
-        HBURST = hbst[0];
-        HWRITE = 1'b0;
-        HSIZE  = hsze[0];
-        HPROT  = hprt[0];
-      end
-      else if (dvalid) begin
-        HTRANS = htns[1];
-        HADDR  = hadr[1];
-        HBURST = hbst[1];
-        HWRITE = hwrt;
-        HSIZE  = hsze[1];
-        HPROT  = hprt[1];
-      end
-    end
-
-    else if (cycle_id == 2'b10) begin
-      if (dburst) begin
-        HTRANS = htns[1];
-        HADDR  = hadr[1];
-        HBURST = hbst[1];
-        HWRITE = hwrt;
-        HSIZE  = hsze[1];
-        HPROT  = hprt[1];
-      end
-      else if (ivalid) begin
-        HTRANS = htns[0];
-        HADDR  = hadr[0];
-        HBURST = hbst[0];
-        HWRITE = 1'b0;
-        HSIZE  = hsze[0];
-        HPROT  = hprt[0];
-      end
-    end
+  HRDATAI <= 32'h0000_0000;
+  HRESPI <= 2'b00;
+  HRDATAD <= 32'h0000_0000;
+  HRESPD <= 2'b00;
+  if (hdtval[0]) begin
+    HRDATAI <= mhrdata;
+    HRESPI <= mhresp;
   end
-  else begin
-    if (iburst) begin
-      HTRANS = BSY;
-      HADDR  = hadr[0];
-      HBURST = hbst[0];
-      HWRITE = 1'b0;
-      HSIZE  = hsze[0];
-      HPROT  = hprt[0];
-    end
-    else if (dburst) begin
-      HTRANS = BSY;
-      HADDR  = hadr[1];
-      HBURST = hbst[1];
-      HWRITE = hwrt;
-      HSIZE  = hsze[1];
-      HPROT  = hprt[1];
-    end
+  if (hdtval[1]) begin
+    HRDATAD <= mhrdata;
+    HRESPD <= mhresp;
   end
-
-  if (cycle_id[1] & hwrt)
-    HWDATA = hwdt;
 end
 
-wire [31:0] dbg_hadr0 = hadr[0];
-wire [31:0] dbg_hadr1 = hadr[1];
-
-always @ (posedge HCLK or negedge HRESETN) begin
+always @ (posedge HCLK) begin
   if (!HRESETN)
-    hinvalid <= 1'b0;
-  else if (HTRANS == BSY)
-    hinvalid <= 1'b1;
-  else
-    hinvalid <= 1'b0;
+    HWDATA <= 32'h0000_0000;
+  else if (HREADY & tbuf[tbrp][1:0] == 2'b10 & tbuf[tbrp][2] == 1'b1)
+    HWDATA <= HWDATAD;
+end
+
+// Mux AHB Cycle Selector
+// ----------------------------------------
+always @ (*) begin
+  if (tbuf[tbrp][1:0] == 2'b01) begin
+    HTRANS = tns[0];
+    HADDR = adr[0];
+    HBURST = bst[0];
+    HWRITE = wrt[0];
+    HSIZE = sze[0];
+    HPROT = prt[0];
+    mhadp = 1;
+  end
+  else if (tbuf[tbrp][1:0] == 2'b10) begin
+    HTRANS = tns[1];
+    HADDR = adr[1];
+    HBURST = bst[1];
+    HWRITE = wrt[1];
+    HSIZE = sze[1];
+    HPROT = prt[1];
+    mhadp = 1;
+  end
+  else begin
+    HTRANS = IDL;
+    HADDR = 32'h0000_0000;
+    HBURST = 3'b000;
+    HWRITE = 1'b0;
+    HSIZE = 3'b000;
+    HPROT = 4'b1111;
+    mhadp = 0;
+  end
+end
+
+always @ (posedge HCLK) begin
+  if (!HRESETN) begin
+    mhdtp <= 0;
+    mhresp <= 2'b00;
+    mhrdata <= 32'h0000_0000;
+  end
+  else if (HREADY) begin
+    mhdtp <= mhadp & (HTRANS != BSY);
+    if (mhdtp) begin
+      mhresp <= HRESP;
+      mhrdata <= HRDATA;
+    end
+  end
 end
 
 endmodule
